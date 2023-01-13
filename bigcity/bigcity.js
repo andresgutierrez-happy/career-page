d702d6ebd5efbe6528270b027898660819480fa0
andresgutierrez-happy <andres@happyagencies.com>
2023-01-13 16:00:54 -0300

    Act .

--- bigcity/bigcity.js
+++ bigcity/bigcity.js
@@ -1,36 +1,32 @@
-let images = document.querySelectorAll(".inactive")
-let prevBtn = document.querySelector(".prev-btn")
-let nextBtn = document.querySelector(".next-btn")
+let images = document.querySelectorAll(".inactive");
+let prevBtn = document.querySelector(".prev");
+let nextBtn = document.querySelector(".next");
 let count = 0;
 
 function next() {
-    
-        move(next)
-    
+  move(next);
 }
 
 function prev() {
-    move(prev)
+  move(prev);
 }
 prevBtn.addEventListener("click", prev);
 nextBtn.addEventListener("click", next);
-    
-            function move(btn) {
-                images[count].classList.remove("active")
-                if (btn === next){
-                    if (count < images.length - 1) {
-                    count++
-                    } else {
-                        count = 0;
-                    }
-                } else if ( btn === prev) {
-                    if (count > 0) {
-                        count--
-                    }else{
-                        count = images.length -1;
-                    }
-                    
-                }
-                images[count].classList.add("active")
-            }
 
+function move(btn) {
+  images[count].classList.remove("active");
+  if (btn === next) {
+    if (count < images.length - 1) {
+      count++;
+    } else {
+      count = 0;
+    }
+  } else if (btn === prev) {
+    if (count > 0) {
+      count--;
+    } else {
+      count = images.length - 1;
+    }
+  }
+  images[count].classList.add("active");
+}
